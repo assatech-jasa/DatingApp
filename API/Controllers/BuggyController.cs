eb0549541bbf6612764aed7b9ae4ed854f6115f3
Jasmin C <jasmin@assatechnology.com>
2021-01-25 21:39:55 -0600

    end of section 7

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using API.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+
+        public BuggyController(DataContext context)
+        {
+            _context=context;
+        }
+
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        //[Authorize]
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = _context.Users.Find(-1);
+
+            if(thing==null)
+            {
+                return NotFound();
+            }
+            return Ok(thing);
+        }
+
+
+        //[Authorize]
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+
+
+            var thingToReturn = thing.ToString();
+
+            return thingToReturn;
+
+        }
+
+
+        //[Authorize]
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("this was not a good request.");
+        }
+
+
+       
+
+    }
+}
