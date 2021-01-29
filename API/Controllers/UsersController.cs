9a8cfd02c194d3e965dd14a3cb710163209a55d9
Jasmin C <jasmin@assatechnology.com>
2021-01-29 09:59:36 -0600

    end of section 9

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -32,7 +32,7 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            var users = _userRepository.GetMembersAsync();
+            var users = await _userRepository.GetMembersAsync();
 
             return Ok(users);
         }
